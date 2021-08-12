48374da8da1dcb806b746287f553dd83f8bb3cd8
Krystof Dolejsi <KrystofDolejsi@gmail.com>
2021-08-12 21:50:24 +0200

    major changes, restructuring the whole page, creating a mobile version. Switch to mobile first developement possible in near future

--- app/app.js
+++ app/app.js
@@ -1,25 +1,112 @@
+//Global Variables needed for Navbar
+let burger = document.querySelector(".navbar-toggle");
+let mobileMenu = document.querySelector(".mobile-nav-links");
+let sections = document.querySelectorAll("section");
+let navbarLinks = document.querySelectorAll("ul.links");
+
+//functions for opening and closing the Navbar menu
+//function to open menu by clicking burger
+const toggleMenu = () => {
+    burger.addEventListener('click', function() {
+        mobileMenu.classList.toggle("mobile-nav-active");
+    });
+};
+
+toggleMenu();
+
+//function to close the menu when clicking elsewhere
+sections.forEach((section) => {
+    section.addEventListener('click', function() {
+        mobileMenu.classList.remove("mobile-nav-active");
+    });
+});
+
+//function to close menu after clicking a button
+navbarLinks.forEach((link) => {
+    link.addEventListener('click', function() {
+        mobileMenu.classList.remove("mobile-nav-active");
+    });
+});
+
+
+//==================================
+//Burger menu transformation effect
+//==================================
+const burgerTransform = () => {
+    //variables
+    let topBar = document.querySelector("span.bar#top");
+    let midBar = document.querySelector("span.bar#mid");
+    let botBar = document.querySelector("span.bar#bot");
+
+    //function to transform the burger when clicking the burger
+    burger.addEventListener('click', function() {
+        topBar.classList.toggle("close");
+        midBar.classList.toggle("close");
+        botBar.classList.toggle("close");
+    });
+
+    //function to transform the burger when clicking elsewhere
+    sections.forEach((section) => {
+        section.addEventListener("click", function() {
+            topBar.classList.remove("close");
+            midBar.classList.remove("close");
+            botBar.classList.remove("close");
+        });
+    });
+
+    //function to transform burger when clicking a button
+
+    navbarLinks.forEach((link) => {
+        link.addEventListener("click", function() {
+            topBar.classList.remove("close");
+            midBar.classList.remove("close");
+            botBar.classList.remove("close");
+        });
+    });
+}
+burgerTransform();
+//===========================================
+
+
 // Nacteni hlavni stranky
 const loadHero = () => {
-    const introNav = document.querySelector("nav");
+    // selector All with for cycle used due to having two separate navbars, 
+    // one for PC and one for mobile
+    const navbarLogo = document.querySelectorAll(".navbar-logo");
 
-    window.addEventListener("load", function() {
-        introNav.style.cssText = "transform: translateY(0%)";
+    navbarLogo.forEach(logo => {
+        window.addEventListener("load", function() {
+        logo.style.cssText = "transform: translateX(0%)";
+        });
     });
 };
 loadHero();
 
 // Postupne zobrazeni textu na hlavni strance
-// const loadIntroText = () => {
-//     const introTextH3 = document.querySelector(".intro-text h3");
-//     const introTextH1 = document.querySelector(".intro-text h1");
-//     const introTextp = document.querySelector(".intro-text p");
+const loadIntroText = () => {
 
-//     setTimeout(function(){ introTextH3.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1100);
-//     setTimeout(function(){ introTextH1.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1200);
-//     setTimeout(function(){ introTextp.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1300);
-// };
+    //navbar links
+    const homeLink = document.querySelector(".home-link");
+    const servicesLink = document.querySelector(".services-link");
+    const aboutLink = document.querySelector(".about-link");
+    const contactLink = document.querySelector(".contact-link");
 
-// loadIntroText();
+    setTimeout(function(){ homeLink.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1100);
+    setTimeout(function(){ servicesLink.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1200);
+    setTimeout(function(){ aboutLink.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1300);
+    setTimeout(function(){ contactLink.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1400);
+
+    //main hero text
+    const introTextH1 = document.querySelector(".sticker-text h1");
+    const introTextH2 = document.querySelector(".sticker-text h2");
+    const heroContactButton = document.querySelector(".hero-button h3");
+
+    setTimeout(function(){ introTextH1.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1100);
+    setTimeout(function(){ introTextH2.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1200);
+    setTimeout(function(){ heroContactButton.style.cssText = "transform: translateY(0%); opacity: 1"; }, 1300);
+};
+
+loadIntroText();
 
 // postupne zobrazeni textu pri scrollovani strankou
 const textAppearOnScroll = () => {
@@ -50,104 +137,9 @@ const textAppearOnScroll = () => {
 };
 textAppearOnScroll();
 
-
-// scroll animace z vybraneho mista na druhe
-function smoothScroll(target, duration) {
-    let scrollHere = document.querySelector(target);
-    let targetPosition = scrollHere.getBoundingClientRect().top+window.pageYOffset;
-    let startPosition = window.pageYOffset;
-    let distance = targetPosition - startPosition;
-    let startTime = null;
-
-    function animationScroll(currentTime) {
-        if(startTime === null) startTime = currentTime;
-        let timeElapsed = currentTime - startTime;
-        let run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, run);
-        if(timeElapsed < duration) requestAnimationFrame(animationScroll);
-    };
-
-    function ease(t, b, c, d) {
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
-    };
-
-    requestAnimationFrame(animationScroll);
-}
-
-//pridani scroll animace jednotlivym linkum
-const scrollToLink = () => {
-    let navbarHome = document.querySelector(".home-link");
-
-    navbarHome.addEventListener("click", function () {
-        smoothScroll("section#one", 1000);
-    });
-
-    //from navbar services button to services
-    let navbarServices = document.querySelector(".services-link");
-
-    navbarServices.addEventListener("click", function () {
-        smoothScroll("section#two", 1000);
-    });
-
-    //from navbar prices button to prices
-    let navbarPrices = document.querySelector(".prices-link");
-
-    navbarPrices.addEventListener("click", function () {
-        smoothScroll("section#four", 1000);
-    });
-
-    //from navbar contact button to contact
-    let navbarContact = document.querySelector(".contact-link");
-
-    navbarContact.addEventListener("click", function () {
-        smoothScroll("section#six", 1000);
-    });
-
-    //from main page to services
-    // let mainBtn = document.querySelector(".mainBtn");
-
-    // mainBtn.addEventListener("click", function() {
-    //     smoothScroll("section#two", 1000);
-    // });
-
-    //from footer to main page
-    let footerHomeBtn = document.querySelector(".back-main-page");
-
-    footerHomeBtn.addEventListener("click", function() {
-        smoothScroll("section#one", 1000);
-    });
-
-    //from footer to services page
-    let footerServicesBtn = document.querySelector(".back-services");
-
-    footerServicesBtn.addEventListener("click", function () {
-        smoothScroll("section#two", 1000);
-    });
-
-    let footerPricesBtn = document.querySelector(".back-prices");
-
-    //from footer to prices page
-    footerPricesBtn.addEventListener("click", function () {
-        smoothScroll("section#four", 1000);
-    });
-
-    //from footer to Contact page
-    let footerContactBtn = document.querySelector(".back-contact");
-
-    footerContactBtn.addEventListener("click", function () {
-        smoothScroll("section#six", 1000);
-    });
-};
-
-scrollToLink();
-
-
 // postupne zobrazeni obrazku pri scrollu
 const imgAppearOnScroll = () => {
-    const fadedImges = document.querySelectorAll(".service-image-container");
+    const fadedImges = document.querySelectorAll(".faded-image-container");
 
     const appearOptions =  {
         treshold: 1,
@@ -172,8 +164,129 @@ const imgAppearOnScroll = () => {
         imgAppear.observe(fadedImg);
     });
 };
-
 imgAppearOnScroll();
+
+
+// scroll animace z vybraneho mista na druhe
+function smoothScroll(target, duration) {
+    let scrollHere = document.querySelector(target);
+    let navbarPc = document.querySelector("nav.pc-nav");
+    let navbarMobile = document.querySelector("nav.mobile-nav");
+
+    let navbarPcPosition = navbarPc.getBoundingClientRect().height;
+    let navbarMobilePosition = navbarMobile.getBoundingClientRect().height;
+
+    // determines if there is mobile or PC navbar, to asign the correct height
+    if(navbarPcPosition === 0) {
+        navbarPosition = navbarMobilePosition;
+    } else {
+        navbarPosition = navbarPcPosition;
+    };
+
+    let targetPosition = scrollHere.getBoundingClientRect().top+window.pageYOffset-navbarPosition;
+    let startPosition = window.pageYOffset;
+    let distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animationScroll(currentTime) {
+        if(startTime === null) startTime = currentTime;
+        let timeElapsed = currentTime - startTime;
+        let run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration) requestAnimationFrame(animationScroll);
+    };
+
+    function ease(t, b, c, d) {
+        t /= d/2;
+        if (t < 1) return c/2*t*t + b;
+        t--;
+        return -c/2 * (t*(t-2) - 1) + b;
+    };
+
+    requestAnimationFrame(animationScroll);
+}
+
+//pridani scroll animace jednotlivym linkum
+const scrollToLink = () => {
+    let navbarHome = document.querySelectorAll(".home-link");
+
+
+    navbarHome.forEach((element) => {
+        element.addEventListener('click', function() {
+            smoothScroll("section#main-page", 1000);
+        });
+    });
+
+    //from navbar services button to services
+    let navbarServices = document.querySelectorAll(".services-link");
+
+    navbarServices.forEach((element) => {
+        element.addEventListener('click', function() {
+            smoothScroll("section#services-section-one", 1000);
+        });
+    });
+
+    //from navbar about button to about
+    let navbarAbout = document.querySelectorAll(".about-link");
+
+    navbarAbout.forEach((element) => {
+        element.addEventListener('click', function() {
+            smoothScroll("section#four", 1000);
+        });
+    });
+
+    //from navbar contact button to contact
+    let navbarContact = document.querySelectorAll(".contact-link");
+
+    navbarContact.forEach((element) => {
+        element.addEventListener('click', function() {
+            smoothScroll("section#contact-section", 1000);
+        });
+    });
+
+    // from main page to contact
+    let heroContactButton = document.querySelector(".hero-button");
+
+    heroContactButton.addEventListener("click", function() {
+        smoothScroll("section#contact-section", 1000);
+    });
+
+    let heroServicesButton = document.querySelector(".border-button-polygon");
+
+    heroServicesButton.addEventListener("click", function() {
+        smoothScroll("section#two", 1000);
+    });
+
+    //from footer to main page
+    let footerHomeBtn = document.querySelector(".back-main-page");
+
+    footerHomeBtn.addEventListener("click", function() {
+        smoothScroll("section#main-page", 1000);
+    });
+
+    //from footer to services page
+    let footerServicesBtn = document.querySelector(".back-services");
+
+    footerServicesBtn.addEventListener("click", function () {
+        smoothScroll("section#services-section-one", 1000);
+    });
+
+    let footerPricesBtn = document.querySelector(".back-prices");
+
+    //from footer to prices page
+    footerPricesBtn.addEventListener("click", function () {
+        smoothScroll("section#four", 1000);
+    });
+
+    //from footer to Contact page
+    let footerContactBtn = document.querySelector(".back-contact");
+
+    footerContactBtn.addEventListener("click", function () {
+        smoothScroll("section#contact-section", 1000);
+    });
+};
+
+scrollToLink();
 
 
 
