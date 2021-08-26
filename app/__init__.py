fd7220a364910ea623ce647acafc4464ba16500d
Krystof Dolejsi <KrystofDolejsi@gmail.com>
2021-08-26 21:21:14 +0200

    added form styles and simple flask backend to validate the form and redirect user to a confirmation page. Next up will be to finish the backend to send email to the user

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask
-from flask import render_template, url_for, flash, redirect
+from flask import Flask, render_template, redirect, url_for
+# from flask_mail import Mail, Message
+
+from werkzeug.wrappers import request
 from app.forms import ContactForm
 
 app = Flask(__name__)
@@ -9,7 +11,12 @@ app.config["SECRET_KEY"] = "000abcdefghijk123456000"
 @app.route("/", methods=["POST", "GET"])
 def home():
     form = ContactForm()
-    if form.validate_on_submit:
-        return render_template("index.html", form=form)
+
+    if form.validate_on_submit():
+        return redirect(url_for("form_submit"))
     return render_template("index.html", form=form)
 
+
+@app.route("/message_sent", methods=["POST", "GET"])
+def form_submit():
+    return render_template("form_submit.html"), {"Refresh": "3, url=http://127.0.0.1:5000/"}
